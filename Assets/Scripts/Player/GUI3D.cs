631c8225dc85c4e17e44799a1c017a1c2dd89f07
Jade <jade_montague@yahoo.com>
2020-09-26 20:28:54 -0500

    Project 01D-Player Feel/Second Update

---
+++ Assets/Scripts/Player/GUI3D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI3D : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject objectToShow;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Places Object 2 Units In Front Of The Camera
+        objectToShow.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, cam.transform.position.z + 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Use This To Have The Object Follow The Camera       
+        //objectToShow.transform.position  = new Vector3(cam.transform.position.x, cam.transform.position.y, cam.transform.position.z + 2);
+
+        //Spins The Object For Demonstration Purposes
+        objectToShow.transform.Rotate(Vector3.up * 2, Space.Self);
+    }
+}
